8823020fd38b02f356e52a8db346770e700a43c6
Ale015 <veadrigojunior@gmail.com>
2024-09-12 00:21:00 -0300

    Remocao dos comentarios não descritivos.

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -37,7 +37,7 @@ namespace DesafioFundamentos.Models
                 } else {
                     if ( validacaoPlaca(plate)){
                         listaDeVeiculos.Add(plate);
-                        Console.WriteLine($"O veículo de placa {plate} foi adicionado com sucesso ao estacionamento.");
+                        Console.WriteLine($"O veículo de placa {plate} foi adicionado com sucesso ao estacionamento.\n");
                         break;
                     } else {
                         Console.WriteLine($"O veículo não foi adicionado ao estacionamento. \n Tente novamente");
@@ -59,7 +59,7 @@ namespace DesafioFundamentos.Models
             bool loopRemove = true;
 
             if(listaDeVeiculos.Count <= 0){
-                Console.WriteLine("Sinto muito, não existem carros estacionados para poder remover.");
+                Console.WriteLine("Sinto muito, não existem carros estacionados para poder remover.\n");
             }else{ 
                 while(loopRemove == true){
 
@@ -99,83 +99,5 @@ namespace DesafioFundamentos.Models
             Console.WriteLine("Pressione uma tecla para continuar: ");
             Console.ReadKey();
         }
-
-
-
-
-
-
-
-
-
-
-
-
     }
-
-
-    // public class Estacionamento
-    // {
-    //     private decimal precoInicial = 0;
-    //     private decimal precoPorHora = 0;
-    //     private List<string> veiculos = new List<string>();
-
-    //     public Estacionamento(decimal precoInicial, decimal precoPorHora)
-    //     {
-    //         this.precoInicial = precoInicial;
-    //         this.precoPorHora = precoPorHora;
-    //     }
-
-    //     public void AdicionarVeiculo()
-    //     {
-    //         // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
-    //         // *IMPLEMENTE AQUI*
-    //         Console.WriteLine("Digite a placa do veículo para estacionar:");
-    //     }
-
-    //     public void RemoverVeiculo()
-    //     {
-    //         Console.WriteLine("Digite a placa do veículo para remover:");
-
-    //         // Pedir para o usuário digitar a placa e armazenar na variável placa
-    //         // *IMPLEMENTE AQUI*
-    //         string placa = "";
-
-    //         // Verifica se o veículo existe
-    //         if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
-    //         {
-    //             Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
-
-    //             // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
-    //             // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
-    //             // *IMPLEMENTE AQUI*
-    //             int horas = 0;
-    //             decimal valorTotal = 0; 
-
-    //             // TODO: Remover a placa digitada da lista de veículos
-    //             // *IMPLEMENTE AQUI*
-
-    //             Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
-    //         }
-    //         else
-    //         {
-    //             Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
-    //         }
-    //     }
-
-    //     public void ListarVeiculos()
-    //     {
-    //         // Verifica se há veículos no estacionamento
-    //         if (veiculos.Any())
-    //         {
-    //             Console.WriteLine("Os veículos estacionados são:");
-    //             // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
-    //             // *IMPLEMENTE AQUI*
-    //         }
-    //         else
-    //         {
-    //             Console.WriteLine("Não há veículos estacionados.");
-    //         }
-    //     }
-    // }
 }
